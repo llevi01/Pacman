c7f2509e60cbde3cf746eb16e5cc6fb55ccd099c
Lénárt Levente <levente.lenart97@gmail.com>
2023-11-20 21:24:06 +0100

    Declare methods in Ghost class

--- src/pacman/game/entity/ghost/Ghost.java
+++ src/pacman/game/entity/ghost/Ghost.java
@@ -1,35 +1,86 @@
 package pacman.game.entity.ghost;
 
+import pacman.game.entity.Direction;
 import pacman.game.entity.Entity;
+import pacman.game.entity.pacman.Pacman;
+import pacman.game.tile.Coordinate;
+import pacman.game.util.Config;
+
+import java.util.ArrayList;
 
 public abstract class Ghost extends Entity {
-    public Ghost(String id) {
+    public Ghost(String id, Pacman pacman) {
         super(id);
+        this.pacman = pacman;
         init();
         updateCurrentTile();
+
+        for (int i = 0; i < 3; i++) {
+            nextStates.add(GhostState.CHASE);
+            nextStates.add(GhostState.SCATTER);
+        }
+        GhostState lastState = GhostState.CHASE;
+        lastState.setInfiniteRemainingTime();
+        nextStates.add(lastState);
     }
 
     /**
      * A szellem aktuális állapota
      */
-    protected GhostState state = GhostState.CHASE;
+    protected GhostState state = GhostState.SCATTER;
+    protected ArrayList<GhostState> nextStates = new ArrayList<>();
+    protected Coordinate target;
 
-    protected GhostState nextState;
+    protected final Coordinate inFrontOfGhostHouse = new Coordinate(0,0);
 
-    protected int DEFAULT_SPEED = 4;
+    // Pacman referenciája
+    protected Pacman pacman;
+
+    protected int DEFAULT_SPEED = 4 * Config.SCALE;
     protected int FRIGHTENED_SPEED = DEFAULT_SPEED / 2;
     protected int EATEN_SPEED = DEFAULT_SPEED * 2;
+
+    @Override
+    public void update() {
+
+    }
 
     /**
      * Frissíti a szellem állapotát
      * A játék elején a szellemek a CHASE és SCATTER
-     * állapotok között váltanak
+     * állapotok között váltanak, majd CHASE állapotban maradnak
      */
     protected void updateState() {
+
+    }
+
+    public void toFrightenedState() {
 
     }
 
     public void toEatenState() {
 
     }
+
+    protected void updateDirection() {
+
+    }
+
+    private boolean inNoTurnZone() {
+        return false;
+    }
+
+    protected void updateTargetTile() {
+
+    }
+
+    protected boolean isInsideGhostHouse() {
+        return false;
+    }
+
+    protected ArrayList<Direction> getValidMoves() {
+        return new ArrayList<>();
+    }
+
+    protected abstract Coordinate getChaseTarget();
 }
--- src/pacman/game/entity/ghost/GhostState.java
+++ src/pacman/game/entity/ghost/GhostState.java
@@ -1,8 +1,31 @@
 package pacman.game.entity.ghost;
 
 public enum GhostState {
-    CHASE,
-    SCATTER,
-    FRIGHTENED,
-    EATEN
+    CHASE(20),
+    SCATTER(7),
+    FRIGHTENED(20),
+    EATEN(-1);
+
+    /**
+     *
+     */
+    private int remainingTime;
+    GhostState(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+    public void decreaseRemainingTime() {
+        remainingTime--;
+    }
+    public boolean isOver() {
+        return remainingTime == 0;
+    }
+    public void setInfiniteRemainingTime() {
+        remainingTime = -1;
+    }
+    public boolean isInfinite() {
+        return remainingTime == -1;
+    }
 }
