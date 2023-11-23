7a2189f94e07b35a6fe47756957cc269e6032e79
Lénárt Levente <levente.lenart97@gmail.com>
2023-11-23 13:35:25 +0100

    Update map

--- src/pacman/game/Game.java
+++ src/pacman/game/Game.java
@@ -76,14 +76,14 @@ public class Game {
         entities = new ArrayList<>();
         Pacman pacman = new Pacman();
         Blinky blinky = new Blinky(pacman);
-        Pinky pinky = new Pinky(pacman);
-        Inky inky = new Inky(pacman, blinky);
-        Clyde clyde = new Clyde(pacman);
+        //Pinky pinky = new Pinky(pacman);
+        //Inky inky = new Inky(pacman, blinky);
+        //Clyde clyde = new Clyde(pacman);
 
         entities.add(pacman);
         entities.add(blinky);
-        entities.add(pinky);
-        entities.add(inky);
-        entities.add(clyde);
+        //entities.add(pinky);
+        //entities.add(inky);
+        //entities.add(clyde);
     }
 }
