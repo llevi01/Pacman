1ac54c1869dd76ee0c4eb1615745edd9d68c1d88
Levente Lénárt <levente.lenart97@gmail.com>
2024-06-16 21:51:39 +0200

    Add Configurable class

---
+++ src/pacman/game/util/Configurable.java
@@ -0,0 +1,12 @@
+package pacman.game.util;
+
+/**
+ * Azon beállítások gyűjteménye, melyeket be lehet állítani a config.json fileban
+ */
+public class Configurable {
+    public String playerName;
+    public String serverAddress;
+    public int scale;
+
+    public Configurable() {}
+}
