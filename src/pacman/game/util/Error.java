7f3850ad79b84d24fb4e6310ce5a65529bc9dd7e
Levente Lénárt <levente.lenart97@gmail.com>
2024-06-16 21:53:32 +0200

    Add new errors

--- src/pacman/game/util/Error.java
+++ src/pacman/game/util/Error.java
@@ -7,7 +7,9 @@ public enum Error {
     LOADING_SPRITES("Failed to load sprites"),
     LOADING_MAP("Failed to load map"),
     LOADING_LEADERBOARD("Failed to load leaderboard"),
-    SAVING_LEADERBOARD("Failed to save leaderboard");
+    SAVING_LEADERBOARD("Failed to save leaderboard"),
+    CONNECTING_TO_SERVER("Failed to connect to server"),
+    LOADING_CONFIG("Failed to load configuration");
     public final String message;
     Error(String message) {
         this.message = message;
